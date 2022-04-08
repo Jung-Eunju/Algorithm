e880c1375c80c2184c59d57467579ff05b036b6e
Jung-Eunju <78429930+Jung-Eunju@users.noreply.github.com>
2022-04-08 14:17:34 +0900

    Create step_5.py

---
+++ step_5.py
@@ -0,0 +1,52 @@
+#15596
+def solve(a):
+    sum=0
+    for i in a:
+        sum+=i
+    return sum
+
+#4673 다시풀기!
+'''
+def d(n):
+    print(n)
+    a=n//10
+    b=n%10
+    n=n+a+b
+    
+    return d(n)
+
+d(1)
+'''
+
+def d(n):
+    n=n+sum(map(int,str(n)))
+    return n
+
+nonSelfNum = set()
+
+for i in range(1,1001):
+    nonSelfNum.add(d(i))
+
+print(nonSelfNum)
+
+for j in range(1,1001):
+    if j not in nonSelfNum:
+        print(j)
+
+
+#1065 다시!
+import sys
+n=int(sys.stdin.readline().rstrip())
+
+
+def han(n):
+    count=0
+    for i in range(1,n+1):
+        num_list = list(map(int,str(i)))
+        if i<100:
+            count+=1
+        elif num_list[0] - num_list[1] == num_list[1] - num_list[2]:
+            count+=1
+    return count
+
+print(han(n))
